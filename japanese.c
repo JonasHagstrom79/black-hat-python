37cf459355b4298aa991b554032d104a65fa0a50
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2024-03-05 20:11:05 +0100

    some c programs

---
+++ japanese.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Japanska tecken för "Hej värld" (こんにちは世界)
+    char greeting[] = "こんにちは世界";
+    
+    printf("Japansk hälsning: %s\n", greeting);
+    
+    return 0;
+}
---
+++ print_url.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("https://www.google.se\n");
+    return 0;
+}
